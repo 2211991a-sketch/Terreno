2d35acfc3129d0117e6e0f3a6a651fbd02209ec1
unknown <22991a@umich.mx>
2026-05-20 12:20:13 -0600

    dbd

---
+++ Assets/SCRIPS/Nave.cs
@@ -0,0 +1,27 @@
+// Movimiento adelante/atrás
+        float move = Input.GetAxis("Vertical");
+
+        // Giro izquierda/derecha
+        float rotate = Input.GetAxis("Horizontal");
+
+        // Subir y bajar
+        float vertical = 0f;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            vertical = 1f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            vertical = -1f;
+        }
+
+        // Movimiento
+        transform.Translate(Vector3.forward * move * moveSpeed * Time.deltaTime);
+
+        // Subir/Bajar
+        transform.Translate(Vector3.up * vertical * verticalSpeed * Time.deltaTime);
+
+        // Rotación
+        transform.Rotate(Vector3.up * rotate * rotationSpeed * Time.deltaTime);
